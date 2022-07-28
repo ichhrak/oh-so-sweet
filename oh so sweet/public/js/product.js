2d75091f51214e707195a45cce868c41c7f96395
ichhrak <77620132+ichhrak@users.noreply.github.com>
2022-07-28 10:04:26 +0100

    Update product.js

--- oh so sweet/public/js/product.js
+++ oh so sweet/public/js/product.js
@@ -1,26 +1,26 @@
-const productImages = document.querySelectorAll(".product-images img"); // selecting all image thumbs
-const productImageSlide = document.querySelector(".image-slider"); // seclecting image slider element
+const productImages = document.querySelectorAll(".product-images img"); 
+const productImageSlide = document.querySelector(".image-slider"); 
 
-let activeImageSlide = 0; // default slider image
+let activeImageSlide = 0; 
 
-productImages.forEach((item, i) => { // loopinh through each image thumb
-    item.addEventListener('click', () => { // adding click event to each image thumbnail
-        productImages[activeImageSlide].classList.remove('active'); // removing active class from current image thumb
-        item.classList.add('active'); // adding active class to the current or clicked image thumb
-        productImageSlide.style.backgroundImage = `url('${item.src}')`; // setting up image slider's background image
-        activeImageSlide = i; // updating the image slider variable to track current thumb
+productImages.forEach((item, i) => { 
+    item.addEventListener('click', () => { 
+        productImages[activeImageSlide].classList.remove('active'); 
+        item.classList.add('active'); 
+        productImageSlide.style.backgroundImage = `url('${item.src}')`; 
+        activeImageSlide = i; 
     })
 })
 
 // toggle size buttons
 
-const sizeBtns = document.querySelectorAll('.size-radio-btn'); // selecting size buttons
+const sizeBtns = document.querySelectorAll('.size-radio-btn'); 
 let checkedBtn = 0; // current selected button
 
-sizeBtns.forEach((item, i) => { // looping through each button
-    item.addEventListener('click', () => { // adding click event to each 
-        sizeBtns[checkedBtn].classList.remove('check'); // removing check class from the current button
-        item.classList.add('check'); // adding check class to clicked button
-        checkedBtn = i; // upading the variable
+sizeBtns.forEach((item, i) => { 
+    item.addEventListener('click', () => { 
+        sizeBtns[checkedBtn].classList.remove('check'); /
+        item.classList.add('check'); 
+        checkedBtn = i; 
     })
 })
